b41237b8d46e75687cc90e1830f787ebc31e8e8c
ZyCromerZ <neetroid97@gmail.com>
2020-03-14 06:04:40 +0700

    add builder for dc

--- a.sh
+++ a.sh
@@ -10,5 +10,21 @@ spectrumFile="vipn.rc"
 # GetBranch=$(git rev-parse --abbrev-ref HEAD)
 GetCommit=$(git log --pretty=format:'%h' -1)
 HeadCommit=$GetCommit
+MainCommit=$GetCommit
 
 build "" "tele" "$chat_group_id"
+build "65Hz" "tele" "$chat_group_id"
+build "71Hz" "tele" "$chat_group_id"
+
+## for pie
+git reset --hard $MainCommit
+git cherry-pick 3f7999d7701391724d59ffb478b2f448a50c2172 babfe8f4448ebd39aaad806ae3eecd1e05960971
+git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e
+
+# FolderUpload="X01BD/KERNEL/DC/STABLE/$(date +"%Y-%m-%d")"
+GetCommit=$(git log --pretty=format:'%h' -1)
+HeadCommit=$GetCommit
+
+build "" "tele" "$chat_group_id"
+build "65Hz" "tele" "$chat_group_id"
+build "71Hz" "tele" "$chat_group_id"
