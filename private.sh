5ea005eb2d02252c9c4991da11bf3d5d6d3c9519
ZyCromerZ <neetroid97@gmail.com>
2020-05-09 00:12:49 +0700

    only build 2 varian

--- private.sh
+++ private.sh
@@ -7,57 +7,57 @@
     . main.sh "get-kernel"
     Getclang
     
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-    MainCommit=$GetCommit
-    chat_id="$chat_group_id"
-    build "QAvalon65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC"
-    build "QAvalon68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
-    git reset --hard $MainCommit
-    git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
-    git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-    build "PAvalon65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
-    build "PAvalon68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
-    clean_build
+#     GetCommit=$(git log --pretty=format:'%h' -1)
+#     HeadCommit=$GetCommit
+#     MainCommit=$GetCommit
+#     chat_id="$chat_group_id"
+#     build "QAvalon65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC"
+#     build "QAvalon68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
+#     git reset --hard $MainCommit
+#     git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
+#     git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
+#     GetCommit=$(git log --pretty=format:'%h' -1)
+#     HeadCommit=$GetCommit
+#     build "PAvalon65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
+#     build "PAvalon68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
+#     clean_build
 
-################################################################################################ AvalonTest
-    change_branch
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-    MainCommit=$GetCommit
-    chat_id="$chat_group_id"
-    build "QAvalonTest65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC"
-    build "QAvalonTest68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
-    git reset --hard $MainCommit
-    git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
-    git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-    build "PAvalonTest65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
-    build "PAvalonTest68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
-    clean_build
+# ################################################################################################ AvalonTest
+#     change_branch
+#     GetCommit=$(git log --pretty=format:'%h' -1)
+#     HeadCommit=$GetCommit
+#     MainCommit=$GetCommit
+#     chat_id="$chat_group_id"
+#     build "QAvalonTest65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC"
+#     build "QAvalonTest68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
+#     git reset --hard $MainCommit
+#     git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
+#     git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
+#     GetCommit=$(git log --pretty=format:'%h' -1)
+#     HeadCommit=$GetCommit
+#     build "PAvalonTest65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
+#     build "PAvalonTest68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
+#     clean_build
 
-################################################################################################ DTC
-    change_branch
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-    MainCommit=$GetCommit
-    chat_id="$chat_group_id"
-    build "QDTC65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC"
-    build "QDTC68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
-    git reset --hard $MainCommit
-    git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
-    git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-    build "PDTC65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
-    build "PDTC68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
-    clean_build
+# ################################################################################################ DTC
+#     change_branch
+#     GetCommit=$(git log --pretty=format:'%h' -1)
+#     HeadCommit=$GetCommit
+#     MainCommit=$GetCommit
+#     chat_id="$chat_group_id"
+#     build "QDTC65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC"
+#     build "QDTC68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
+#     git reset --hard $MainCommit
+#     git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
+#     git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
+#     GetCommit=$(git log --pretty=format:'%h' -1)
+#     HeadCommit=$GetCommit
+#     build "PDTC65Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
+#     build "PDTC68Hz" "" "$chat_group_id" "$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)-ZyC" >/dev/null
+#     clean_build
 
-################################################################################################ DTCoLd
-    change_branch
+# ################################################################################################ DTCoLd
+#     change_branch
     GetCommit=$(git log --pretty=format:'%h' -1)
     HeadCommit=$GetCommit
     MainCommit=$GetCommit
