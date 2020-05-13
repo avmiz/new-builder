b25f374e055cb23ba8acd2c80a5c8557dfe04f97
ZyCromerZ <neetroid97@gmail.com>
2020-05-13 14:36:05 +0700

    add eg builder

---
+++ eg-h.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+################################################################################################ EG H DTC
+    branch="eg/20200313/h"
+    folder="all"
+    FolderUpload="X01BD/KERNEL/EG/STABLE/H"
+    spectrumFile="h.rc"
+    . main.sh "get-kernel"
+    Getclang "dtc"
+    Getclang "GCC"
+
+    GetCommit=$(git log --pretty=format:'%h' -1)
+    HeadCommit=$GetCommit
+    MainCommit=$GetCommit
+    chat_id="$chat_group_id"
+    CONFIG_HZ="1000"
+    # sendInfo "starting build QuantumKiller-N . . ."
+
+    build "QDTC60Hz" "" "$chat_group_id"
+    build "QDTC65Hz" "" "$chat_group_id" >/dev/null
+    build "QDTC68Hz" "" "$chat_group_id" >/dev/null
+    build "QDTC71Hz" "" "$chat_group_id" >/dev/null
+
+    # sendInfo "build QuantumKiller-N done . . ."
+
+    ## for pie
+    git reset --hard $MainCommit
+    git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
+    git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
+
+    # FolderUpload="X01BD/KERNEL/EG/STABLE/H"
+    GetCommit=$(git log --pretty=format:'%h' -1)
+    HeadCommit=$GetCommit
+
+    # sendInfo "starting build DeadlyCute-N . . ."
+
+    build "PDTC60Hz" "" "$chat_group_id" >/dev/null
+    build "PDTC65Hz" "" "$chat_group_id" >/dev/null
+    build "PDTC68Hz" "" "$chat_group_id" >/dev/null
+    build "PDTC71Hz" "" "$chat_group_id" >/dev/null
+
+    # sendInfo "build DeadlyCute-N done . . ."
+
+    clean_build
+
+################################################################################################ EG H GCC
+    change_branch
+    GetCommit=$(git log --pretty=format:'%h' -1)
+    HeadCommit=$GetCommit
+    MainCommit=$GetCommit
+    chat_id="$chat_group_id"
+    CONFIG_HZ="100"
+    # sendInfo "starting build QuantumKiller-N . . ."
+
+    build "QGCC60Hz" "" "$chat_group_id"
+    build "QGCC65Hz" "" "$chat_group_id" >/dev/null
+    build "QGCC68Hz" "" "$chat_group_id" >/dev/null
+    build "QGCC71Hz" "" "$chat_group_id" >/dev/null
+
+    # sendInfo "build QuantumKiller-N done . . ."
+
+    ## for pie
+    git reset --hard $MainCommit
+    git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
+    git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
+
+    # FolderUpload="X01BD/KERNEL/EG/STABLE/H"
+    GetCommit=$(git log --pretty=format:'%h' -1)
+    HeadCommit=$GetCommit
+
+    # sendInfo "starting build DeadlyCute-N . . ."
+
+    build "PGCC60Hz" "" "$chat_group_id" >/dev/null
+    build "PGCC65Hz" "" "$chat_group_id" >/dev/null
+    build "PGCC68Hz" "" "$chat_group_id" >/dev/null
+    build "PGCC71Hz" "" "$chat_group_id" >/dev/null
+
+    # sendInfo "build DeadlyCute-N done . . ."
+
+    clean_build
