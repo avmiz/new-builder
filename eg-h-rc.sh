b5f340128198e8543b9465987e32b93c1e7adc56
ZyCromerZ <neetroid97@gmail.com>
2020-05-28 01:32:07 +0700

    update builder

--- eg-h-rc.sh
+++ eg-h-rc.sh
@@ -5,7 +5,7 @@
     FolderUpload="X01BD/KERNEL/EG/Release-Candidate/H"
     spectrumFile="h.rc"
     . main-b.sh "get-kernel"
-    Getclang
+    Getclang 'dtc'
 
     GetCommit=$(git log --pretty=format:'%h' -1)
     HeadCommit=$GetCommit
@@ -16,7 +16,10 @@
 
     build "QDTC60Hz" "" "$chat_group_id"
     build "QDTC65Hz" "" "$chat_group_id" >/dev/null
+    build "QDTC66Hz" "" "$chat_group_id" >/dev/null
+    build "QDTC67Hz" "" "$chat_group_id" >/dev/null
     build "QDTC68Hz" "" "$chat_group_id" >/dev/null
+    build "QDTC69Hz" "" "$chat_group_id" >/dev/null
     build "QDTC71Hz" "" "$chat_group_id" >/dev/null
 
     # sendInfo "build QuantumKiller-N done . . ."
@@ -26,7 +29,7 @@
     git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
     git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
 
-    # FolderUpload="X01BD/KERNEL/EG/Release-Candidate/H"
+    # FolderUpload="X01BD/KERNEL/EG/Release-Candidate/F"
     GetCommit=$(git log --pretty=format:'%h' -1)
     HeadCommit=$GetCommit
 
@@ -34,81 +37,15 @@
 
     build "PDTC60Hz" "" "$chat_group_id" >/dev/null
     build "PDTC65Hz" "" "$chat_group_id" >/dev/null
+    build "PDTC66Hz" "" "$chat_group_id" >/dev/null
+    build "PDTC67Hz" "" "$chat_group_id" >/dev/null
     build "PDTC68Hz" "" "$chat_group_id" >/dev/null
+    build "PDTC69Hz" "" "$chat_group_id" >/dev/null
     build "PDTC71Hz" "" "$chat_group_id" >/dev/null
 
     # sendInfo "build DeadlyCute-N done . . ."
 
     clean_build
 
-################################################################################################ EG H Avalon
-    change_branch
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-    MainCommit=$GetCommit
-    chat_id="$chat_group_id"
-    CONFIG_HZ="300"
-    # sendInfo "starting build QuantumKiller-N . . ."
-
-    build "QAvalon60Hz" "" "$chat_group_id"
-    build "QAvalon65Hz" "" "$chat_group_id" >/dev/null
-    build "QAvalon68Hz" "" "$chat_group_id" >/dev/null
-    build "QAvalon71Hz" "" "$chat_group_id" >/dev/null
-
-    # sendInfo "build QuantumKiller-N done . . ."
-
-    ## for pie
-    git reset --hard $MainCommit
-    git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
-    git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
-
-    # FolderUpload="X01BD/KERNEL/EG/STABLE/M"
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-
-    # sendInfo "starting build DeadlyCute-N . . ."
-
-    build "PAvalon60Hz" "" "$chat_group_id" >/dev/null
-    build "PAvalon65Hz" "" "$chat_group_id" >/dev/null
-    build "PAvalon68Hz" "" "$chat_group_id" >/dev/null
-    build "PAvalon71Hz" "" "$chat_group_id" >/dev/null
-
-    # sendInfo "build DeadlyCute-N done . . ."
-
-    clean_build
-
-################################################################################################ EG H Avalon
-    change_branch
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-    MainCommit=$GetCommit
-    chat_id="$chat_group_id"
-    CONFIG_HZ="300"
-    # sendInfo "starting build QuantumKiller-N . . ."
-
-    build "QProton60Hz" "" "$chat_group_id"
-    build "QProton65Hz" "" "$chat_group_id" >/dev/null
-    build "QProton68Hz" "" "$chat_group_id" >/dev/null
-    build "QProton71Hz" "" "$chat_group_id" >/dev/null
-
-    # sendInfo "build QuantumKiller-N done . . ."
-
-    ## for pie
-    git reset --hard $MainCommit
-    git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
-    git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
-
-    # FolderUpload="X01BD/KERNEL/EG/STABLE/M"
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-
-    # sendInfo "starting build DeadlyCute-N . . ."
-
-    build "PProton60Hz" "" "$chat_group_id" >/dev/null
-    build "PProton65Hz" "" "$chat_group_id" >/dev/null
-    build "PProton68Hz" "" "$chat_group_id" >/dev/null
-    build "PProton71Hz" "" "$chat_group_id" >/dev/null
-
-    # sendInfo "build DeadlyCute-N done . . ."
-
-    clean_build
+cd ..
+rm -rf $folder
--- qk-n-rc.sh
+++ qk-n-rc.sh
@@ -5,8 +5,8 @@
     FolderUpload="X01BD/KERNEL/QK/Release-Candidate/N"
     spectrumFile="vipn.rc"
     . main-b.sh "get-kernel"
-    Getclang
-
+    Getclang 'dtc'
+    
     GetCommit=$(git log --pretty=format:'%h' -1)
     HeadCommit=$GetCommit
     MainCommit=$GetCommit
@@ -16,7 +16,10 @@
 
     build "DTC60Hz" "" "$chat_group_id"
     build "DTC65Hz" "" "$chat_group_id" >/dev/null
+    build "DTC66Hz" "" "$chat_group_id" >/dev/null
+    build "DTC67Hz" "" "$chat_group_id" >/dev/null
     build "DTC68Hz" "" "$chat_group_id" >/dev/null
+    build "DTC69Hz" "" "$chat_group_id" >/dev/null
     build "DTC71Hz" "" "$chat_group_id" >/dev/null
 
     # sendInfo "build QuantumKiller-N done . . ."
@@ -35,83 +38,15 @@
 
     build "DTC60Hz" "" "$chat_group_id" >/dev/null
     build "DTC65Hz" "" "$chat_group_id" >/dev/null
+    build "DTC66Hz" "" "$chat_group_id" >/dev/null
+    build "DTC67Hz" "" "$chat_group_id" >/dev/null
     build "DTC68Hz" "" "$chat_group_id" >/dev/null
+    build "DTC69Hz" "" "$chat_group_id" >/dev/null
     build "DTC71Hz" "" "$chat_group_id" >/dev/null
 
     # sendInfo "build DeadlyCute-N done . . ."
 
     clean_build
 
-################################################################################################ QK N Avalon
-    change_branch
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-    MainCommit=$GetCommit
-    chat_id="$chat_group_id"
-    CONFIG_HZ="300"
-    # sendInfo "starting build QuantumKiller-N . . ."
-
-    build "Avalon60Hz" "" "$chat_group_id"
-    build "Avalon65Hz" "" "$chat_group_id" >/dev/null
-    build "Avalon68Hz" "" "$chat_group_id" >/dev/null
-    build "Avalon71Hz" "" "$chat_group_id" >/dev/null
-
-    # sendInfo "build QuantumKiller-N done . . ."
-
-    ## for pie
-    git reset --hard $MainCommit
-    git cherry-pick 3f7999d7701391724d59ffb478b2f448a50c2172 97a48a19482d88311df54eebcb26ab6da6c8f3c6
-    git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
-    git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
-
-    FolderUpload="X01BD/KERNEL/DC/Release-Candidate/N"
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-
-    # sendInfo "starting build DeadlyCute-N . . ."
-
-    build "Avalon60Hz" "" "$chat_group_id" >/dev/null
-    build "Avalon65Hz" "" "$chat_group_id" >/dev/null
-    build "Avalon68Hz" "" "$chat_group_id" >/dev/null
-    build "Avalon71Hz" "" "$chat_group_id" >/dev/null
-
-    # sendInfo "build DeadlyCute-N done . . ."
-
-    clean_build
-
-################################################################################################ QK N Proton
-    change_branch
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-    MainCommit=$GetCommit
-    chat_id="$chat_group_id"
-    CONFIG_HZ="300"
-    # sendInfo "starting build QuantumKiller-N . . ."
-
-    build "Proton60Hz" "" "$chat_group_id"
-    build "Proton65Hz" "" "$chat_group_id" >/dev/null
-    build "Proton68Hz" "" "$chat_group_id" >/dev/null
-    build "Proton71Hz" "" "$chat_group_id" >/dev/null
-
-    # sendInfo "build QuantumKiller-N done . . ."
-
-    ## for pie
-    git reset --hard $MainCommit
-    git cherry-pick 3f7999d7701391724d59ffb478b2f448a50c2172 97a48a19482d88311df54eebcb26ab6da6c8f3c6
-    git revert ab24c40ba48e47f4543ac9afa9763112a7d3d68e --no-commit
-    git commit -s -m "revert: ab24c40ba48e47f4543ac9afa9763112a7d3d68e"
-
-    FolderUpload="X01BD/KERNEL/DC/STABLE/N"
-    GetCommit=$(git log --pretty=format:'%h' -1)
-    HeadCommit=$GetCommit
-
-    # sendInfo "starting build DeadlyCute-N . . ."
-
-    build "Proton60Hz" "" "$chat_group_id" >/dev/null
-    build "Proton65Hz" "" "$chat_group_id" >/dev/null
-    build "Proton68Hz" "" "$chat_group_id" >/dev/null
-    build "Proton71Hz" "" "$chat_group_id" >/dev/null
-
-    # sendInfo "build DeadlyCute-N done . . ."
-
-    clean_build
+cd ..
+rm -rf $folder
