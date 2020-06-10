961bcc99f821ed09475be8bd718ad49afc45f779
ZyCromerZ <neetroid97@gmail.com>
2020-06-10 07:23:06 +0700

    switch use google clang 9.0.3 only

--- main-c.sh
+++ main-c.sh
@@ -154,7 +154,7 @@ function makeZip(){
     fi
     cp -af anykernel-real.sh anykernel.sh
     sed -i "s/kernel.string=.*/kernel.string=$KERNEL_NAME-$HeadCommit by ZyCromerZ/g" anykernel.sh
-    ZipName="[DTC][$TANGGAL]$ZIP_KERNEL_VERSION-$KERNEL_NAME-$HeadCommit-TEST.zip"
+    ZipName="[GoogleClang9][$TANGGAL]$ZIP_KERNEL_VERSION-$KERNEL_NAME-$HeadCommit.zip"
     zip -r $ZipName ./ -x /.git/**\* ./anykernel-real.sh ./.gitignore ./LICENSE ./README.md ./spectrum/**\* ./*.zip  1>/dev/null 2>/dev/null 2>&1
     if [ "$withPassword" == "YES" ];then
         zip -r --password "$3" "$ZipName-protected.zip" $ZipName 1>/dev/null 2>/dev/null 2>&1
@@ -252,8 +252,8 @@ function Getclang(){
     [ ! -d "Getclang" ] && mkdir Getclang
     cd Getclang
     [ ! -d ".git" ] && git init
-    # setRemote "https://github.com/ZyCromerZ/google-clang.git" "gugel-clang" "9.0.3-r353983c"
-    setRemote "https://github.com/Bikram557/DragonTC-10.0.git" "dtc" "dragontc"
+    setRemote "https://github.com/ZyCromerZ/google-clang.git" "gugel-clang" "9.0.3-r353983c"
+    # setRemote "https://github.com/Bikram557/DragonTC-10.0.git" "dtc" "dragontc"
     cd ..
     [ ! -d "GetGcc" ] && mkdir GetGcc
     cd GetGcc
@@ -266,11 +266,13 @@ function Getclang(){
 }
 function SetClang(){
     cd Getclang
-    # git checkout gugel-clang/9.0.3-r353983c
-    git checkout dtc/dragontc
+    git fetch gugel-clang/9.0.3-r353983c
+    git checkout FETCH_HEAD
+    # git checkout dtc/dragontc
     cd ..
     cd GetGcc
-    git checkout gcc-google/master
+    git fetch gcc-google master
+    git checkout FETCH_HEAD
     cd ..
     # cd GetGccB
     # git fetch gcc-google ndk-r19
