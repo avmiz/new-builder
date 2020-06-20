ec03f6e5bbbd7b1476f2e8fa8c5b5bf226d7ec88
ZyCromerZ <neetroid97@gmail.com>
2020-06-20 09:59:06 +0700

    add pusher to auto build X01BD kernel

---
+++ build-xobod.sh
@@ -0,0 +1,7 @@
+if [ ! -z "$1" ] && [ "$1" == "rc" ];then
+    git push neetroid eg-f-rc eg-m-rc eg-h-rc private-oc-rc -f && \
+    git push bitbucket -f qk-n-rc qk-l-rc qk-ul-rc private-rc
+else
+    git push neetroid eg-f eg-m eg-h private-oc -f && \
+    git push bitbucket -f qk-n qk-l qk-ul private
+fi
