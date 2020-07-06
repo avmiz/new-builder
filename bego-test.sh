e4129ca4ec2418696b13d2e9029d9cdb713ac6eb
ZyCromerZ <neetroid97@gmail.com>
2020-07-06 10:16:37 +0700

    bego-test: update

--- bego-test.sh
+++ bego-test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 ################################################################################################ Memetek
-    branch="20200629/main"
+    branch="20200706/root"
     folder="all"
     FolderUpload="Kernel/Begonia/Tercydux-TEST"
     spectrumFile="bego.rc"
     . main-c.sh "get-kernel"
     ProjectId="zyc-files"
-    TypeBuid="TEST-ORI"
+    TypeBuid="ORI-MAIN"
     Getclang
     SetClang
 
@@ -15,6 +15,14 @@
     MainCommit=$GetCommit
     chat_id="$chat_group_id"
 
+    build "" "tele" "$chat_group_id"
+    branch="20200706/upstream"
+    TypeBuid="ORI-MAIN"
+    clean_build
+    build "" "tele" "$chat_group_id"
+    branch="20200706/upstream-common"
+    TypeBuid="ORI-MAIN"
+    clean_build
     build "" "tele" "$chat_group_id"
 
 cd ..
