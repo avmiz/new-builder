2869d94d6eb15367bf6b01bd9970d91dcc37e7c1
ZyCromerZ <neetroid97@gmail.com>
2020-07-19 09:39:36 +0700

    file.sh: update

--- file.sh
+++ file.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 . main.sh
+git clone https://github.com/MiCode/Xiaomi_Kernel_OpenSource -b begonia-p-oss
+cd Xiaomi_Kernel_OpenSource
+git fetch origin begonia-q-oss
+git checkout origin/begonia-q-oss && git checkout -b begonia-q-oss
+git remote add private https://$githubKey@github.com/ZyCromerZ/begonia_stock.git
+git push --all private -f
+cd ..
+rm -rf Xiaomi_Kernel_OpenSource
